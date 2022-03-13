102a7f12a44ca916207f68b7f68ee21ad16c536c
Franklin Peñaranda <coffemanfp@gmail.com>
2022-03-13 14:09:53 -0500

    Added comments to the errors package

--- errors/client.go
+++ errors/client.go
@@ -2,6 +2,8 @@ package errors
 
 import "fmt"
 
+// ClientError represents a error to present to the client.
+// Implements the error interface.
 type ClientError struct {
 	httpCode int
 	message  string
@@ -11,10 +13,16 @@ func (h ClientError) Error() string {
 	return h.message
 }
 
+// HTTPCode gets the http code of the error. Returns 0 if it's not available.
 func (h ClientError) HTTPCode() int {
 	return h.httpCode
 }
 
+// NewClientError initialices a new error with a ClientError implementation.
+//  @param httpCode: represents the http error code for the http response.
+//  @param m string: message to be presented to the client.
+//  @param a ...interface{}: optional arguments for the message.
+//	@return $1 error: new ClientError error implementation instance.
 func NewClientError(httpCode int, m string, a ...interface{}) error {
 	return ClientError{
 		httpCode: httpCode,
